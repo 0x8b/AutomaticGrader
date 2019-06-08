5335b636c1d873967aeaf2c2f910c75100b496a7
Mariusz Gumienny <temporary.gumienny@gmail.com>
2019-06-08 18:57:00 +0200

    wersja beta

---
+++ app/src/main/java/com/example/android/opencvproject/Tuple.java
@@ -0,0 +1,11 @@
+package com.example.android.opencvproject;
+
+public class Tuple<F, S> {
+    public final F first;
+    public final S second;
+
+    public Tuple(F f, S s) {
+        this.first = f;
+        this.second = s;
+    }
+}
